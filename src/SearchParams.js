8d8e46d51c86240d5394d7c682a28e31590004e8
Melina Lencina <melina00lencina@gmail.com>
2022-08-18 01:57:03 -0300

    :art: Improve format/structure

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -28,8 +28,9 @@ const SearchParams = () => {
   }
 
   return (
-    <div className="search-params">
+    <div className="my-0 mx-auto w-11/12">
       <form
+        className="p-10 mb-10 rounded-lg bg-gray-200 shadow-lg flex flex-col justify-center items-center"
         onSubmit={(e) => {
           e.preventDefault();
           requestPets();
@@ -39,9 +40,11 @@ const SearchParams = () => {
           Location
           <input
             id="location"
+            type="text"
             value={location}
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
+            className="w-60 mb-5 block"
           />
         </label>
         <label htmlFor="animal">
@@ -57,6 +60,7 @@ const SearchParams = () => {
               setAnimal(e.target.value);
               setBreed("");
             }}
+            className="w-60 mb-5 block"
           >
             <option />
             {ANIMALS.map((animal) => (
@@ -74,6 +78,7 @@ const SearchParams = () => {
             value={breed}
             onChange={(e) => setBreed(e.target.value)}
             onBlur={(e) => setBreed(e.target.value)}
+            className="w-60 mb-5 block disabled:opacity-50"
           >
             <option />
             {breeds.map((breed) => (
@@ -89,6 +94,7 @@ const SearchParams = () => {
             value={theme}
             onChange={(e) => setTheme(e.target.value)}
             onBlur={(e) => setTheme(e.target.value)}
+            className="w-60 mb-5 block"
           >
             <option value="peru">Peru</option>
             <option value="crimson">Crimson</option>
@@ -96,7 +102,12 @@ const SearchParams = () => {
             <option value="mediumorchid">Medium Orchid</option>
           </select>
         </label>
-        <button style={{ backgroundColor: theme }}>Submit</button>
+        <button
+          className="rounded px-6 py-2 color text-white hover:opacity-50 border-none"
+          style={{ backgroundColor: theme }}
+        >
+          Submit
+        </button>
       </form>
       <Results pets={pets} />
     </div>
